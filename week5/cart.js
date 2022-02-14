c6e02986c4680e84e671aaa8b0d263d7ed63e27a
Jmimiding4104 <Jmimiding4104@gmail.com>
2022-02-15 02:19:04 +0800

    init

---
+++ week5/cart.js
@@ -0,0 +1,10 @@
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js";
+
+const app = createApp({
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+});
+app.mount("#app");
